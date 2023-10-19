3675dd9a6fce94c4741231a5628dce98093b5552
BURAK\beyte <beyterburak@gmail.com>
2023-10-19 20:01:48 +0300

    added: Validation

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -11,7 +11,7 @@ import { ProductService } from 'src/app/services/common/models/product.service';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent extends BaseComponent {
-  constructor(spinner: NgxSpinnerService ,private productService: ProductService, private alertify: AlertifyService) { 
+  constructor(spinner: NgxSpinnerService, private productService: ProductService, private alertify: AlertifyService) {
     super(spinner);
   }
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
@@ -21,6 +21,23 @@ export class CreateComponent extends BaseComponent {
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
+    if (!name.value) {
+      this.alertify.message("Lütfen ürün adını giriniz.", {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      })
+      return;
+    }
+
+    if (parseInt(stock.value) < 0) {
+      this.alertify.message("Lütfen stok bilgisini doğru giriniz.", {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      })
+    }
+
     this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.BallAtom);
       this.alertify.message("Ürün başarıyla eklenmiştir.", {
@@ -28,6 +45,12 @@ export class CreateComponent extends BaseComponent {
         messageType: MessageType.Success,
         position: Position.TopRight
       })
-    }); 
+    }, errorMessage => {
+      this.alertify.message(errorMessage, {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+    });
   }
 }
